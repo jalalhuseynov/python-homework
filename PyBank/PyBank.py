5ddab899a630142d1db6a80ff54519f6de350381
Jay Hashman <jayhashman@Jays-MacBook-Pro.local>
2021-11-20 22:02:27 -0500

    README.md file updated, PyBank code modified, exporting text file added

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,12 +1,20 @@
+# Importing the pathlib and csv library
+
 from pathlib import Path
 import csv
 
+# Setting the file path
 csvpath = Path('budget_data.csv')
+
+# Openning the csv file as an object
 
 with open (csvpath, 'r') as csvfile:
     
+    # csv file to the csv.reader() function
+
     csvreader = csv.reader(csvfile, delimiter=',')
     
+    # Readding the header row
     csv_header = next(csvreader)
     
    
@@ -14,7 +22,6 @@ with open (csvpath, 'r') as csvfile:
     
     month_count = 0
     total_pnl = 0
-    monthly_difference = []
     greatest_increase = 0
     greatest_increase_month = ""
     greatest_decrease = 0
@@ -23,31 +30,28 @@ with open (csvpath, 'r') as csvfile:
     total = 0
     month = 0
     
+    #Initializing a list
+    monthly_difference = []
+    
+    # Read each row of data after the header
     for row in csvreader:
      
         #number of months
-        month_count += 1
+        month_count = month_count + 1
         #total profit/loss calculation
         total_pnl += int(row[1])
         
-        if row[0] == "Date":
-            pass
-        elif row[0] == "Jan-2010":
+        #checking month by month to calculate the avearage change
+        if  row[0] == "Jan-2010":
             previous_month = int(row[1])
-            month_count += 1
+            month += 1
             total += int(row[1])
             greatest_increase = 0
             greatest_increase_month = row[0]
             greatest_decrease = 0
             greatest_decrease_month = row[0]
-        elif row[0] == "Feb-2010":
-            greatest_increase = int(row[1]) - previous_month
-            greatest_decrease = int(row[1]) - previous_month
-            month += 1
-            total += int(row[1])
-            greatest_increase_month = row[0]
-            monthly_difference.append(int(row[1]) - previous_month)
-            previous_month = int(row[1])
+            
+       #finding the greatest increase and the greates decrease
         else:
             month += 1
             total += int(row[1])
@@ -62,10 +66,25 @@ with open (csvpath, 'r') as csvfile:
             
             previous_month = int(row[1])
             
+    # Printing final results: total months, total pnl, avearage change, greatest increase and greatest decrease   
     print("Financial Analysis")
-    print("-----------------------")
-    print(f"Total Months: {month_count-1}")
+    print("_________________________")
+    print(f"Total Months: {month_count}")
     print(f"Total PnL: ${total_pnl}")
-    print(f"Average Change: {round(sum(monthly_difference)/85, 2)} ")
+    print(f"Average Change: {round(sum(monthly_difference)/len(monthly_difference), 2)} ")
     print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
     print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
+    
+# Setting output file name    
+result_path = 'results.txt'
+
+# Openning the output path as a file object
+with open(result_path, 'w') as file:
+    
+    file.write("Financial Analysis")
+    file.write("_________________________")
+    file.write(f"Total Months: {month_count}")
+    file.write(f"Total PnL: ${total_pnl}")
+    file.write(f"Average Change: {round(sum(monthly_difference)/len(monthly_difference), 2)} ")
+    file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+    file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-homework
-This is a second homework in Fintech class. 
+## This is a second homework in Fintech class.
+
+PyBank folder has jupyter notebook file called main.ipynb and python script called PyBank.py
+
+Solutions can be seen by running PyBank.py script. 
+After running, the script will export a text file with the results called results.txt
